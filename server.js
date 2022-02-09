fab6f1ee66557118c6bd534db19a14636330019b
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-09 03:25:33 -0500

    connected server.js to sql database

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'rootpassword',
+      database: 'myEmployees'
+    },
+    console.log('Connected to the myEmployees database.')
+  );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

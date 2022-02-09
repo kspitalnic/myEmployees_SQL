9a89946dfe748ac4ac5e57a54c0d2e7323a8c621
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-09 03:24:09 -0500

    default response for other requests

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
   });
 
-  
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });

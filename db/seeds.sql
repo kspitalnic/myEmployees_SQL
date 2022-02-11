652284b50d52a151f49761980b636931737660a0
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-10 23:11:39 -0500

    updated tables for id numbers to represent role/employee/department

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,29 +4,26 @@ VALUES
     ('HR'),
     ('IT'),
     ('Management'),
-    ('Sales'),
-    ('Sales'),
-    ('Sales'),
     ('Admin');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Lead Salesman', 85.00, 100),
-    ('HR Admin', 54.00, 101),
-    ('Technician', 85.50 ,102),
-    ('Manager', 100.00, 103),
-    ('Salesman', 70.50, 104),
-    ('Sales Intern', 55.00, 105),
-    ('Salesman', 58.00, 106),
-    ('Secretary', 33.00, 107);
+    ('Manager', 100.00, 4),
+    ('Lead Salesman', 85.00, 1),
+    ('Salesman', 70.50, 5),
+    ('HR Admin', 54.00, 2),
+    ('Technician', 85.50 ,3),
+    ('Secretary', 33.00, 8);
+    ('Sales Intern', 55.00, 7),
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Amy', 'Adams', 202, NULL),
-    ('Ben', 'Smith', 101, NULL),
-    ('Carly', 'Doe', 404, NULL),
-    ('Dave', 'Smith', 606, 258),
-    ('Edward', 'Jones', 303, NULL),
-    ('Frank', 'Hills', 101, NULL),
-    ('Gianna', 'Pitt', 101, NULL),
-    ('Emily', 'Bates', 105, NULL)
+    ('Amy', 'Adams', 1, NULL),
+    ('Ben', 'Smith', , NULL),
+    ('Carly', 'Doe', 2, NULL),
+    ('Dave', 'Smith', 4, 258),
+    ('Edward', 'Jones', 5, NULL),
+    ('Frank', 'Hills', , NULL),
+    ('Gianna', 'Pitt', , NULL),
+    ('Emily', 'Bates', , NULL)
--- server.js
+++ server.js
@@ -10,71 +10,73 @@ app.use(express.json());
 
 // Connect to database
 const connection = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'rootpassword',
-      database: 'myEmployees'
-    },
-    console.log('Connected to the myEmployees database.'),
-    mainMenu()
-  );
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpassword',
+    database: 'myEmployees'
+  },
+  console.log('Connected to the myEmployees database.'),
+  mainMenu()
+);
 
 function mainMenu() {
   inquirer
     .prompt({
-      type:'list',
-      name:'menu',
-      message:'What would you like to do?',
-      choices:[
+      type: 'list',
+      name: 'menu',
+      message: 'What would you like to do?',
+      choices: [
         'View all departments',
         'View all roles',
         'View all employees',
         'Add a department',
         'Add a role',
         'Add an employee',
-        'Update an employee role',
-        "Exit"
-      ]
+        'Update an employee role'
+        ]
     })
-    .then(function({ menu }) {
+    .then(function ({ menu }) {
       if (menu = 'View all departments') {
         viewDepartments();
-        return;
       }
       else if (menu = 'View all roles') {
         viewRoles();
-        return;
       }
       else if (menu = 'View all employees') {
         viewEmployees();
-        return;
       }
       else if (menu = 'Add a department') {
         addDepartment();
-        return;
       }
       else if (menu = 'Add a role') {
         addRole();
-        return;
       }
       else if (menu = 'Add an employee') {
         addEmployee();
-        return;
       }
       else if (menu = 'Update an employee role') {
         updateRole();
-        return;
       }
     })
 };
- 
+
 function viewDepartments() {
   connection.query('SELECT * FROM department', function (err, results) {
-    console.log(results);
-  });};
+    console.table(results);
+  });
+  mainMenu();
+};
 
-  function viewRoles() {
-    connection.query('SELECT * FROM role', function (err, results) {
-      console.log(results);
-    });}
+function viewRoles() {
+  connection.query('SELECT * FROM role', function (err, results) {
+    console.table(results);
+  });
+  mainMenu();
+}
+
+function viewEmployees() {
+  connection.query('SELECT * FROM employee', function (err, results) {
+    console.table(results);
+  }).then(mainMenu())
+}

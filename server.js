2cc3328dd6fae814590624e7bc3ce4069ca48d51
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-10 22:49:53 -0500

    function to view departments

--- server.js
+++ server.js
@@ -2,29 +2,79 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
-
+var inquirer = require('inquirer');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
       password: 'rootpassword',
       database: 'myEmployees'
     },
-    console.log('Connected to the myEmployees database.')
+    console.log('Connected to the myEmployees database.'),
+    mainMenu()
   );
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
+function mainMenu() {
+  inquirer
+    .prompt({
+      type:'list',
+      name:'menu',
+      message:'What would you like to do?',
+      choices:[
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        "Exit"
+      ]
+    })
+    .then(function({ menu }) {
+      if (menu = 'View all departments') {
+        viewDepartments();
+        return;
+      }
+      else if (menu = 'View all roles') {
+        viewRoles();
+        return;
+      }
+      else if (menu = 'View all employees') {
+        viewEmployees();
+        return;
+      }
+      else if (menu = 'Add a department') {
+        addDepartment();
+        return;
+      }
+      else if (menu = 'Add a role') {
+        addRole();
+        return;
+      }
+      else if (menu = 'Add an employee') {
+        addEmployee();
+        return;
+      }
+      else if (menu = 'Update an employee role') {
+        updateRole();
+        return;
+      }
+    })
+};
+ 
+function viewDepartments() {
+  connection.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+  });};
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  function viewRoles() {
+    connection.query('SELECT * FROM role', function (err, results) {
+      console.log(results);
+    });}
